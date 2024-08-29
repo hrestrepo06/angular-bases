148322b5068466e0bc1ec013ee6c0cfcdbf10689
Henry <hrest.06@gmail.com>
2024-08-29 14:31:16 -0500

    fin de seccion 4

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrl: './heroe.component.css'
+})
+export class HeroeComponent {
+
+  public name: string = 'ironman';
+  public age:  number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroeDescription(): string {
+    return `${ this.name } - ${ this.age }`
+  }
+
+  changeHeroe(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
+
+
+}
